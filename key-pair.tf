8b998137056a9ce8e5ae021d4c64bef1d077e33c
Ekkachai Danwanichakul <ek@prattle.co>
2017-11-08 11:21:18 -0600

    update key-pair name variable

--- key-pair.tf
+++ key-pair.tf
@@ -3,7 +3,7 @@ module "ssh" {
   source                = "git::https://github.com/cloudposse/terraform-aws-key-pair.git?ref=master"
   namespace             = "aws"
   stage                 = "dev"
-  name                  = "aws-senario2"
+  name                  = "${var.infra_name}"
   ssh_public_key_path   = "./secrets"
   generate_ssh_key      = "true"
   private_key_extension = ".pem"
